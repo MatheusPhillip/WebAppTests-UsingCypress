57cf447fe3d27098f38a965991f44d276882a73a
MatheusMiranda <mpssm@cesar.org.br>
2023-03-09 15:59:55 -0300

    Added code to create, view and delete a transaction

--- cypress/e2e/web/functional.cy.js
+++ cypress/e2e/web/functional.cy.js
@@ -37,7 +37,7 @@ describe("Should test at functional level", () =>{
 
     it('Should update an account', function ()  {
         cy.accessAccountMenu()
-        cy.xpath(locators.ACCOUNTS.XP_BTN_UPDATE).click()
+        cy.xpath(locators.ACCOUNTS.FN_XP_BTN_UPDATE(Cypress.env('account_name'))).click()
         cy.get(locators.ACCOUNTS.NAME_INPUT_FIELD)
             .clear()
             .type(Cypress.env('account_name_updated'))
@@ -49,7 +49,7 @@ describe("Should test at functional level", () =>{
 
     it('Should delete an account', function () {
         cy.accessAccountMenu()
-        cy.xpath(locators.ACCOUNTS.XP_UPDATED_ACCOUNT_BTN_UPDATED).click()
+        cy.xpath(locators.ACCOUNTS.FN_XP_BTN_DELETE(Cypress.env('account_name_updated'))).click()
         cy.fixture('toast_success_messages').as('toast').then(() => {
             cy.get(locators.TOAST_MESSAGE).should('contain', this.toast.account_deleted)
         })
@@ -71,6 +71,40 @@ describe("Should test at functional level", () =>{
         cy.fixture('toast_error_messages').as('toast').then(() =>{
             cy.get(locators.TOAST_MESSAGE).should('contain', this.toast.duplicated_account)
         })
+    })
+
+    it('Should create a transaction', function() {
+        cy.get(locators.MENU.TRANSACTION).click()
+
+        // CREATING A TRANSACTION
+        cy.get(locators.ACCOUNT_TRANSACTION.DESCRIPTION).type('Desc')
+        cy.get(locators.ACCOUNT_TRANSACTION.VALUE).type('123')
+        cy.get(locators.ACCOUNT_TRANSACTION.INTERESTED).type('Bank name')
+        cy.get(locators.ACCOUNT_TRANSACTION.ACCOUNT).select(Cypress.env('account_name'))
+        cy.get(locators.ACCOUNT_TRANSACTION.STATUS).click()
+        
+        // CONFIRMING THE TRANSACTION
+        cy.get(locators.ACCOUNT_TRANSACTION.BTN_SAVE).click()
+
+        // VERIFYING THE MESSAGE
+        cy.fixture('toast_success_messages').as('toast').then(() => {
+            cy.get(locators.TOAST_MESSAGE).should('contain', this.toast.transaction)
+        })
+
+        // VERIFYING IF THE NEW TRANSACTION WAS RECORDED
+        cy.get(locators.EXTRACT.LINES).should('have.length', 7)
+        cy.xpath(locators.EXTRACT.FN_XP_SEARCH_TRANSACTION('Desc', '123,00')).should('exist')
+    })
+
+    it('Should get balance', function() {
+        cy.get(locators.MENU.HOME).click()
+        cy.xpath(locators.BALANCE.FN_XP_ACCOUNT_BALANCE(Cypress.env('account_name'))).should('contain', '123,00')
+    })
+
+    it('Should delete a transaction', function() {
+        cy.get(locators.MENU.EXTRACT).click()
+        cy.xpath(locators.EXTRACT.FN_XP_DELETE_TRANSACTION('Desc')).click()
+        
     })
     
 })
--- cypress/support/locators.js
+++ cypress/support/locators.js
@@ -6,16 +6,46 @@ const locators = {
         BTN_LOGIN: '.btn'
     },
     MENU: {
+        // HOME PAGE OPTION
+        HOME: '[data-test="menu-home"]',
+
+        // EXTRACT OPTION
+        EXTRACT: '[data-test="menu-extrato"]',
+
+        // SETTINGS OPTION
         SETTINGS: '[data-test="menu-settings"]',
+
+        // THESE OPTIONS ARE INSIDE THE SETTINGS
         ACCOUNTS_OPTION: '[href="/contas"]',
         RESET_OPTION: '[href="/reset"]',
-        LOGOUT_OPTION: '[href="/logout"]'
+        LOGOUT_OPTION: '[href="/logout"]',
+
+        // TRANSACTION OPTION
+        TRANSACTION: '[data-test="menu-movimentacao"]'
+        
     },
     ACCOUNTS:{
         NAME_INPUT_FIELD: '[data-test="nome"]',
         BTN_SAVE: '.btn',
-        XP_BTN_UPDATE: `//table//td[contains(., \'${Cypress.env('account_name')}\')]/..//i[@class='far fa-edit']`,
-        XP_UPDATED_ACCOUNT_BTN_UPDATED: `//table//td[contains(., \'${Cypress.env('account_name_updated')}\')]/..//i[@class='far fa-trash-alt']`
+        FN_XP_BTN_UPDATE: (account_name) => `//table//td[contains(., '${account_name}')]/..//i[@class='far fa-edit']`,
+        FN_XP_BTN_DELETE: (account_name) => `//table//td[contains(., '${account_name}')]/..//i[@class='far fa-trash-alt']`
+    },
+    ACCOUNT_TRANSACTION: {
+        DESCRIPTION: '[data-test="descricao"]',
+        VALUE: '[data-test="valor"]',
+        INTERESTED: '[data-test="envolvido"]',
+        BTN_SAVE: '.btn-primary',
+        STATUS: '[data-test="status"]',
+        ACCOUNT: '[data-test="conta"]'
+    },
+    EXTRACT: {
+        LINES: '.list-group > li',
+        FN_XP_SEARCH_TRANSACTION: (desc, value) => `//span[contains(., '${desc}')]/following-sibling::small[contains(., '${value}')]`,
+        FN_XP_DELETE_TRANSACTION: desc => `//span[contains(., '${desc}')]/../../..//i[@class='far fa-trash-alt']`
+    },
+    BALANCE: {
+        FN_XP_ACCOUNT_BALANCE: (account_name) => `//td[contains(., '${account_name}')]/../td[contains(., '123,00')]`
+        //XP_ACCOUNT_BALANCE: `//td[contains(., 'test account')]/../td[contains(., '123,00')]`
     },
     TOAST_MESSAGE: '.toast-message'
 
